9ac657f4ed572ee9bb08048b56128da9c5c265ca
Tyler <tpiwowarski3@gatech.edu>
2023-03-23 15:51:30 -0400

    Added basic new book functionality

--- script.js
+++ script.js
@@ -1,1 +1,113 @@
+// Load in the book container
+const bookContainer = document.querySelector('.book-container');
+
+// Load in New Book button
+const newBookButton = document.querySelector('#new-book-button');
+
+// Define an array to store books
+let myLibrary = [];
+
+// Define a Book constructor
+function Book(title, author, pageNum, hasRead) 
+{
+    this.title = title
+    this.author = author
+    this.pageNum = pageNum
+    this.hasRead = hasRead
+}
+
+// Function to add a new book to library
+function addBookToLibrary(book)
+{
+    myLibrary.push(book);
+}
+
+// Function to create a div containing a book and its information
+function createBookCard(book)
+{
+    // Create a div for the book to hold all info
+    let bookDiv = document.createElement('div');
+
+    // Give a class to the div
+    bookDiv.classList.add('book');
+
+    // Create child divs for: title, author, pageNum, hasRead
+    let bookTitle = document.createElement('div');
+    bookTitle.setAttribute('id', 'book-card-title');
+    bookTitle.textContent = book.title;
+
+    let bookAuthor = document.createElement('div');
+    bookAuthor.setAttribute('id', 'book-card-author');
+    bookAuthor.textContent = book.author;
+
+    let bookNumPages = document.createElement('div');
+    bookNumPages.setAttribute('id', 'book-card-num-pages');
+    bookNumPages.textContent = book.pageNum;
+
+    let bookHasRead = document.createElement('div');
+    bookHasRead.setAttribute('id', 'book-card-has-read');
+    bookHasRead.textContent = book.hasRead;
+
+    // Append the info divs to the bookDiv
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookNumPages);
+    bookDiv.appendChild(bookHasRead);
+
+    // Append the newly created bookDiv to the bookContainer
+    bookContainer.appendChild(bookDiv);
+
+}
+
+// Function to loop through myLibrary and display the books on the page
+function displayBooks(bookList)
+{
+    // Iterate over the books in our library
+    for (let i = 0; i < bookList.length; i++)
+    {
+        createBookCard(bookList[i]);
+    }
+}
+
+// New Book Button Functionality
+newBookButton.addEventListener("click", newBookButtonClick, false);
+
+function newBookButtonClick(event)
+{
+    event.preventDefault();
+
+    const form = document.querySelector('form');
+    
+    // Get user inputs
+    const titleInput = document.querySelector('#title');
+    const authorInput = document.querySelector('#author');
+    const numPagesInput = document.querySelector('#num-pages');
+    const readInput = document.querySelector('#read');
+
+    // Create a new Book object
+    let newBook = new Book(titleInput.value, authorInput.value, numPagesInput.value, readInput.value);
+
+    // Create a new card for the book
+    createBookCard(newBook);
+
+    // Add the book to our library
+    addBookToLibrary(newBook);
+
+    // Reset form after click
+    form.reset();
+
+    
+}
+
+// Create some books to add to myLibrary
+const book1 = new Book('book1', 'tyler', '223', 'yes');
+const book2 = new Book('book2', 'aaron', '125', 'no');
+const book3 = new Book('book3', 'mike', '412', 'yes');
+
+// Add books to myLibrary
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+displayBooks(myLibrary);
 
